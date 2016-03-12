0382d8f334a265d575358d43873cbafff2232654
Spiegel <spiegel.im.spiegel@gmail.com>
2016-03-12 19:03:03 +0900

    add CountZunDoko() function.

--- zundoko.go
+++ zundoko.go
@@ -14,25 +14,39 @@ const (
 
 //Choirs - zundoko-choirs list
 type Choirs struct {
-	c  []string
-	ct int
+	c []string
 }
 
 //Push is append choirs
 func (c *Choirs) Push(s string) {
 	c.c = append(c.c, s)
-	c.ct++
 }
 
-//Count returns choirs count
-func (c *Choirs) Count() int {
-	return c.ct
+//CountZunDoko returns count of "ZUN" and "DOKO" choirs
+func (c *Choirs) CountZunDoko() (int, int) {
+	z := 0
+	d := 0
+	if c == nil {
+		return z, d
+	}
+	for _, s := range c.c {
+		switch s {
+		case Zun:
+			z++
+		case Doko:
+			d++
+		}
+	}
+	return z, d
 }
 
 func (c *Choirs) String() string {
+	if c == nil {
+		return ""
+	}
 	content := make([]byte, 0, 128)
-	for _, str := range c.c {
-		content = append(content, str...)
+	for _, s := range c.c {
+		content = append(content, s...)
 	}
 	return string(content)
 }
@@ -52,7 +66,7 @@ func generate() chan string {
 //Run zundoko-choirs
 func Run() *Choirs {
 	zd := generate()
-	var c = &Choirs{make([]string, 0), 0}
+	var c = &Choirs{make([]string, 0)}
 	zcount := 0
 	for {
 		s := <-zd
